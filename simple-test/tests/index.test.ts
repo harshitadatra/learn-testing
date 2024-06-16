78652dff72f500dbb7bae350bf185039bbba4a55
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2024-06-16 22:36:36 +0530

    multiply test added

--- simple-test/tests/index.test.ts
+++ simple-test/tests/index.test.ts
@@ -1,8 +1,13 @@
 import { describe, expect, test } from "@jest/globals";
-import { sum } from "../src";
+import { product, sum } from "../src";
 
 describe("sum module", () => {
   test("adds 1 +2 equal to 3", () => {
     expect(sum(1, 2)).toBe(3);
+  });
+});
+describe("multipley module", () => {
+  test("2 multiply by 3 equal to 6", () => {
+    expect(product(2, 3)).toBe(6);
   });
 });
